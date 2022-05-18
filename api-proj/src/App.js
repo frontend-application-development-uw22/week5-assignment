41849596fc48dafd4abcbe7ac9ea7bd8aba6887a
RainyCityDiver <cvmembership@posteo.net>
2022-05-18 13:20:57 -0700

    Added fetch to api.

---
+++ api-proj/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Home from './Home';
+
+function App() {
+  return (
+    <div className="">
+      <Home character='1' />
+    </div>
+  );
+}
+
+export default App;
---
+++ api-proj/src/Home.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react'
+// import PropTypes from 'prop-types'
+
+function Home({ character }) {
+
+  useEffect(() => {
+    fetch(`https://swapi.dev/api/people/${character}`)
+      // .then(reply => reply.json())
+      // .then(data => console.log(data))
+  })
+
+  return (
+    <div>Profile for {character}</div>
+  )
+}
+
+// Home.propTypes = {}
+
+export default Home
