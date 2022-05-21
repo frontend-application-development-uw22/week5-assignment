e31934c93acb8976d995bb08feda7e2a8461ea58
Joann Cahill <cahilljm@aol.com>
2022-05-21 14:18:00 -0700

    Push CharDetail working with embedded url

--- star-wars-app/src/components/CharDetails.jsx
+++ star-wars-app/src/components/CharDetails.jsx
@@ -1,24 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState} from "react";
 import luke from "../images/luke-skywalker.jpg"
-import { Link  } from 'react-router-dom';
-
+// import {Link} from 'react-router-dom';
 
 
 function CharDetails() {
-    return (
-        <div>
-            <p><Link to="/">Return to Home</Link></p>
-            <img className ="character-img" src={luke} alt="Luke Skywalker" />
-            <h2>Luke Skywalker</h2>
+    const [charDetail, setCharDetail] = useState(undefined);
+    const [isLoading, setIsLoading] = useState(true);
+    const [hasError, setHasError] = useState(false);
 
-            <p><span>Hair Color:  </span>blond</p>
-            <p><span>Eye Color:  </span>blue</p>
-            <p><span>Height:  </span>177 cm</p>
+  useEffect(() => {
+     fetch("https://swapi.dev/api/people/3/")
+
+        .then(response => response.json())
+
+        .then(
+            data => {
+            setCharDetail(data);
+            setIsLoading(false);
+            },
+            // .error handling
+            error => {
+            setHasError(true)
+            setIsLoading(false);
+
+            }
+        );
 
 
-        </div>
+    }, []);
 
-    )
+    // end of useEffect
+
+
+    if (isLoading) {
+        return <p>Loading...</p>
+    }
+    // end of isLoading
+
+    if (hasError) {
+        return <p>An error has occurred.  Please try again.</p>
+    }
+
+    // end of hasError
+
+  return ( 
+    <div>
+        <img className = "character-img" src = {luke}alt = "Luke Skywalker" / >
+        <h2 > {charDetail.name}</h2>
+        <p > <span> Hair Color: </span>{charDetail.hair_color}</p>
+        <p> <span> Eye Color: </span>{charDetail.eye_color}</p>
+        <p > <span> Height: </span>{charDetail.height} cm</p>
+    </div>
+  )
 
 }
 
