503c1ed8e9c49ebe00ba17b027b79f26385c4415
RainyCityDiver <cvmembership@posteo.net>
2022-05-21 13:00:23 -0700

    Added search to pg..

--- api-proj/src/App.js
+++ api-proj/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 import Home from './Home';
+import { useState } from 'react';
 
 function App() {
+  const [searchInput, setSearchInput] = useState('');
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    setSearchInput(event.target[0].value);
+  };
+
   return (
     <div className="">
-      <Home character='Luke' />
+      <form onSubmit={onFormSubmit}>
+        <label htmlFor="search-input">Search: </label>
+        <input id="search-input" />
+        <button type="Submit">Find Character</button>
+      </form>
+      {searchInput && <Home character={searchInput} />}
     </div>
   );
 }
--- api-proj/src/Home.jsx
+++ api-proj/src/Home.jsx
@@ -1,16 +1,56 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 // import PropTypes from 'prop-types'
 
 function Home({ character }) {
+  const [profile, setProfile] = useState(undefined);
+  const [isLoading, setIsLoading] = useState(true);
+  const[hasError, setHasError] = useState(false);
+
+  // For practice:
+  // useEffect(() => {
+  //   const getCharacter = async () => {
+  //     try { 
+  //       const reply = await fetch(`https://swapi.dev/api/people/?search=${character}`);
+  //       const data= await reply.json();
+  //       setProfile(data.results[0]);
+  //       setIsLoading(false);
+  //     } catch {
+  //       setHasError(true);
+  //       setIsLoading(false);
+  //     }
+  //   }
+  //   getCharacter();
+  // });
 
   useEffect(() => {
     fetch(`https://swapi.dev/api/people/?search=${character}`)
       .then(reply => reply.json())
-      .then(data => console.log(data))
-  })
+      .then(
+        data => {
+          setIsLoading(false);
+          setProfile(data.results[0])
+        },
+        // Error handling here to avoid .catch{}-ing unrelated errors
+        error => {
+          setHasError(true);
+          setIsLoading(false);
+        }
+      )
+      // Need character in dependency list so useEffect() re-runs if/when it changes
+  }, [character]);
+
+  if (isLoading) {
+    return <div>Obtaining data...</div>
+  }
+
+  if (hasError) {
+    <div>Sorry, an error occured, please try again later.</div>
+  }
 
   return (
-    <div>Profile for {character}</div>
+    <div>
+      <h2>{profile.name}</h2>
+    </div>
   )
 }
 
