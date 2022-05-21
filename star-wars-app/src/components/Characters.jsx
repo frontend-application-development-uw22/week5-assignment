2b7de30a3f4d3b40591122a0634026d940acd756
Joann Cahill <cahilljm@aol.com>
2022-05-20 21:56:41 -0700

    Push baseline

--- star-wars-app/src/components/Characters.jsx
+++ star-wars-app/src/components/Characters.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import luke from "../images/luke-skywalker.jpg";
+// import luke from "../images/luke-skywalker.jpg";
 
 
 function Characters({character}) {
@@ -8,7 +8,7 @@ function Characters({character}) {
     return (
       
             <div className="character-list">
-                <img className="character-img" src={luke} alt="Luke holding a light sabre" />
+                {/* <img className="character-img" src={luke} alt="Luke holding a light sabre" /> */}
                 <p>{character.name}</p>
                 <button>Learn More</button>
             </div>
