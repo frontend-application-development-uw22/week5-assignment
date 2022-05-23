4c36d283d399ec5509fb8396ab54f7f15b3cc4b9
RainyCityDiver <cvmembership@posteo.net>
2022-05-23 11:16:08 -0700

    .map() might be working now.

--- swchars/src/CharList.jsx
+++ swchars/src/CharList.jsx
@@ -1,33 +1,59 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react';
 // import PropTypes from 'prop-types'
 
+
 export default function CharList() {
+  // const [character, setCharacter] = useState('');
+  // function addChar (name) {setCharacter(...name, name)}
+  
   useEffect(() => {
     fetch(`https://swapi.dev/api/people/`)
       .then(reply => reply.json())
-      .then(data => {
+      .then(data => data.results)
+      .then(results => results.map(character => {
+        console.log(character.name);
         return (
-          <>
-            {data.results.map((character) => (
-              <div 
-                key={character.height}
-                className="card m-2 p-2"
-              >
-                <div className="card-body">
-                  <div className="card-title">{character.name}</div>
+          <div className="card m-2 p-2">
+              <div className="card-body">
+                <div className="card-title">
+                  {character.name}
                 </div>
               </div>
-            ))}
-          </>
-        )
-      }
-      );
+          </div>
+        );
+      }))
   })
-  return (
-    <h1>Character List</h1>
-  )
-}
+        // {addChar(data.results.map((character, index)));
+      // console.log(jsonData)
+      // const dataResults = jsonData.results;
+      // for (let i = 0; i < dataResults.length; i++) {
+      //   // const element = dataResults[i];
+      //   return (
+      // )})
+      // {jsonData.map((name, index) => {addChar(jsonData.results.index)})
+        // setCharacter(jsonData.results) 
+      }
+    //   )
+    // }
+    
+  //   return (
+  //     <div>
+  //       {/* {data.results.map((character, index) => ( */}
+  //         <div 
+  //         key={character.index}
+  //         className="card m-2 p-2"
+  //         >
+  //           <div className="card-body">
+  //             <div className="card-title">
+  //               {character.name}
+  //             </div>
+  //           </div>
+  //         </div>
+  //       {/* ))} */}
+  //     </div>
+  //   );
+  // }
 
 // CharList.propTypes = {
-//   charName: PropTypes.string.isRequired,
+//   charName: PropTypes.string.isRequired
 // }
