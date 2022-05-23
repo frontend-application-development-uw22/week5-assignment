bd3aafa873d49462a6353c827e91487ff15c5d3e
RainyCityDiver <cvmembership@posteo.net>
2022-05-23 12:57:22 -0700

    Now getting character names in page as HTML.

--- swchars/src/CharList.jsx
+++ swchars/src/CharList.jsx
@@ -3,27 +3,24 @@ import React, { useEffect, useState } from 'react';
 
 
 export default function CharList() {
-  // const [character, setCharacter] = useState('');
+  const [character, setCharacter] = useState([]);
   // function addChar (name) {setCharacter(...name, name)}
   
   useEffect(() => {
     fetch(`https://swapi.dev/api/people/`)
       .then(reply => reply.json())
       .then(data => data.results)
-      .then(results => results.map(character => {
-        console.log(character.name);
-        return (
-          <div className="card m-2 p-2">
-              <div className="card-body">
-                <div className="card-title">
-                  {character.name}
-                </div>
-              </div>
-          </div>
-        );
-      }))
-  })
-        // {addChar(data.results.map((character, index)));
+      .then(results => setCharacter([...results, character]))
+      
+        // <div className="card m-2 p-2">
+          //     <div className="card-body">
+          //       <div className="card-title">
+          //         {character.name}
+          //       </div>
+          //     </div>
+          // </div>
+        
+          // {addChar(data.results.map((character, index)));
       // console.log(jsonData)
       // const dataResults = jsonData.results;
       // for (let i = 0; i < dataResults.length; i++) {
@@ -32,27 +29,31 @@ export default function CharList() {
       // )})
       // {jsonData.map((name, index) => {addChar(jsonData.results.index)})
         // setCharacter(jsonData.results) 
-      }
+  }, [])
     //   )
     // }
     
-  //   return (
-  //     <div>
-  //       {/* {data.results.map((character, index) => ( */}
-  //         <div 
-  //         key={character.index}
-  //         className="card m-2 p-2"
-  //         >
-  //           <div className="card-body">
-  //             <div className="card-title">
-  //               {character.name}
-  //             </div>
-  //           </div>
-  //         </div>
-  //       {/* ))} */}
-  //     </div>
-  //   );
-  // }
+    return (
+      <div>
+        {character.map((char, index) => {
+          return(
+            <h1 key={index}>{char.name}</h1>
+          )})}
+      </div> 
+
+      //   <div 
+      //     key={character.index}
+      //     className="card m-2 p-2"
+      //     >
+      //       <div className="card-body">
+      //         <div className="card-title">
+      //           {character.name}
+      //         </div>
+      //       </div>
+      //     </div>
+      //   {/* ))} 
+    )
+}
 
 // CharList.propTypes = {
 //   charName: PropTypes.string.isRequired
